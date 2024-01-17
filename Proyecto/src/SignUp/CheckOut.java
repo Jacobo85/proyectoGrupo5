3bc6c92684e4b1b2658171fc0d7e182e00ea39ed
Jacobo85 <153738573+Jacobo85@users.noreply.github.com>
2024-01-17 20:23:51 +0100

    Actualización del proyecto
    En esta actualización se han hecho pequeñas modificaciones sobre la apariencia de las ventanas y su codificación.
    A parte de esto se ha implementado la conexión con la base de datos proyectoif (ubicada en mi localhost) y operaciones como añadir usuarios a la tabla registrousuarios y la tabla infousuarios.
    De esta manera hacemos funcional el alta de nuevos usuarios (sign up) y su información.
    El sign in también queda funcional, ya que se hace una comprobación con la tabla registrousuarios del e-mail y la contraseña para asegurar que sea un usuario registrado el que va a acceder al Main Menu
    Las tablas infousuarios y registrousuarios estan relacionadas por clave primaria y foránea.

--- Proyecto/src/SignUp/CheckOut.java
+++ Proyecto/src/SignUp/CheckOut.java
@@ -23,8 +23,16 @@ import javax.swing.border.EtchedBorder;
 
 public class CheckOut extends JFrame {
 
-	JFrame CheckOut;
+	private JFrame CheckOutFrame;
 	private JLabel sliderValueLabel;
+	private JPanel panel;
+	private JLabel fianzaLabel;
+	private JLabel titleLabel;
+	private JLabel imageLabel;
+	private JSlider slider;
+	private JLabel pagoLabel;
+	private RoundButton ad1Button;
+	private RoundButton nextButton;
 
 	/**
 	 * Launch the application.
@@ -34,7 +42,7 @@ public class CheckOut extends JFrame {
 			public void run() {
 				try {
 					CheckOut window = new CheckOut();
-					window.CheckOut.setVisible(true);
+					window.CheckOutFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -53,120 +61,127 @@ public class CheckOut extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		CheckOut = new JFrame();
-		CheckOut.setBounds(100, 100, 450, 300);
-		CheckOut.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		CheckOutFrame = new JFrame();
+		CheckOutFrame.setBounds(100, 100, 450, 300);
+		CheckOutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		// Crear un panel (JPanel) para agregar contenido.
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
 
         //Establecer el color de fondo del panel en amarillo.
         panel.setBackground(Color.YELLOW);
 
         //Agregar el panel a la ventana.
-        CheckOut.getContentPane().add(panel, BorderLayout.CENTER);
-           panel.setLayout(null);
+        CheckOutFrame.getContentPane().add(panel, BorderLayout.CENTER);
+        panel.setLayout(null);
            
         // Crear una etiqueta "Fianza Establecida:"
-           JLabel fianzaLabel = new JLabel("Fianza Establecida Mensual:");
-           fianzaLabel.setFont(new Font("SansSerif", Font.BOLD, 13));
-           fianzaLabel.setBounds(28, 69, 256, 14);
-           panel.add(fianzaLabel);
+        fianzaLabel = new JLabel("Fianza Establecida Mensual:");
+        fianzaLabel.setFont(new Font("SansSerif", Font.BOLD, 13));
+        fianzaLabel.setBounds(28, 69, 256, 14);
+        panel.add(fianzaLabel);
            
         //Crear una etiqueta (JLabel) para el t�tulo "Check Out!"
-           JLabel titleLabel = new JLabel("Check Out!");
-           titleLabel.setBounds(151, 11, 133, 32);
-           titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
-           titleLabel.setForeground(Color.BLACK); // Establece el color del texto
-           panel.add(titleLabel); 
+        titleLabel = new JLabel("Check Out!");
+        titleLabel.setBounds(151, 11, 133, 32);
+        titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+        titleLabel.setForeground(Color.BLACK); // Establece el color del texto
+        panel.add(titleLabel); 
            
            
-           // Crea una etiqueta para mostrar la imagen
-                 JLabel imageLabel = new JLabel();
-                 imageLabel.setBounds(362, 11, 64, 82); // Ajusta las coordenadas y dimensiones seg�n tus necesidades
-                 panel.add(imageLabel);
-            // Ruta de la imagen
-                 String imagePath = "LOGO CLOUDTOWN-27.png"; // Reemplaza con la ruta correcta de tu imagen
-      try {
-                 // Carga la imagen
-                 ImageIcon imageIcon = new ImageIcon(imagePath);
-                 Image image = imageIcon.getImage();
+        // Crea una etiqueta para mostrar la imagen
+        imageLabel = new JLabel();
+        imageLabel.setBounds(362, 11, 64, 82); // Ajusta las coordenadas y dimensiones seg�n tus necesidades
+        panel.add(imageLabel);
+        // Ruta de la imagen
+        String imagePath = "LOGO CLOUDTOWN-27.png"; // Reemplaza con la ruta correcta de tu imagen
+        try {
+        	
+            // Carga la imagen
+            ImageIcon imageIcon = new ImageIcon(imagePath);
+            Image image = imageIcon.getImage();
 
-                 // Ajusta el tama�o de la imagen
-                 Image newImage = image.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH);
+            // Ajusta el tama�o de la imagen
+            Image newImage = image.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH);
 
-                 // Actualiza la etiqueta con la nueva imagen
-      imageLabel.setIcon(new ImageIcon(newImage));
-      }catch (Exception ex) {
+            // Actualiza la etiqueta con la nueva imagen
+            imageLabel.setIcon(new ImageIcon(newImage));
+        }catch (Exception ex) {
           ex.printStackTrace();
-      }
-           
-         
+        }
            
         // Crear el slider
-           JSlider slider = new JSlider(JSlider.HORIZONTAL,0, 100, 50);
-           slider.setForeground(new Color(0, 0, 0));
-           slider.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-           slider.setFont(new Font("Tahoma", Font.PLAIN, 8));
-           slider.setBounds(38, 89, 300, 35);
-           slider.setMajorTickSpacing(10);
-           slider.setMinorTickSpacing(5);
-           slider.setPaintTicks(true);
-           slider.setPaintLabels(true);
-           panel.add(slider);
+        slider = new JSlider(JSlider.HORIZONTAL,0, 100, 50);
+        slider.setForeground(new Color(0, 0, 0));
+        slider.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+        slider.setFont(new Font("Tahoma", Font.PLAIN, 8));
+        slider.setBounds(38, 89, 300, 35);
+        slider.setMajorTickSpacing(10);
+        slider.setMinorTickSpacing(5);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        panel.add(slider);
           
         //Creo la etiqueta para guardar valor del slider   
-           sliderValueLabel = new JLabel("Su fianza Anual es de : 150");
-           sliderValueLabel.setFont(new Font("SansSerif", Font.BOLD, 13));
-           sliderValueLabel.setBounds(28, 150, 200, 14);
-           panel.add(sliderValueLabel);
+        sliderValueLabel = new JLabel("Su fianza Anual es de : 150");
+        sliderValueLabel.setFont(new Font("SansSerif", Font.BOLD, 13));
+        sliderValueLabel.setBounds(28, 150, 200, 14);
+        panel.add(sliderValueLabel);
            
-       //Crear la eqtiqueta del pago  
-           JLabel pagoLabel = new JLabel("Upload Pago");
-           pagoLabel.setFont(new Font("SansSerif", Font.BOLD, 13));
-           pagoLabel.setBounds(28, 175, 163, 26);
-           panel.add(pagoLabel);
+        //Crear la eqtiqueta del pago  
+        pagoLabel = new JLabel("Upload Pago");
+        pagoLabel.setFont(new Font("SansSerif", Font.BOLD, 13));
+        pagoLabel.setBounds(28, 175, 163, 26);
+        panel.add(pagoLabel);
            
-         //creo boton adjuntar 1
-           RoundButton ad1Button = new RoundButton("Adjuntar");
-           ad1Button.setBackground(Color.GRAY);
-           ad1Button.setForeground(new Color(0, 0, 0));
-           ad1Button.setFont(new Font("SansSerif", Font.BOLD, 6));
-           ad1Button.addActionListener(new ActionListener() {
+        //creo boton adjuntar 1
+        ad1Button = new RoundButton("Adjuntar");
+        ad1Button.setBackground(Color.GRAY);
+        ad1Button.setForeground(new Color(0, 0, 0));
+        ad1Button.setFont(new Font("SansSerif", Font.BOLD, 6));
+        ad1Button.addActionListener(new ActionListener() {
            	public void actionPerformed(ActionEvent e) {
            	}          	
            	//Aqui codigo del evento al pulsar el boton
            });
-           ad1Button.setBounds(67, 215, 59, 14);
-           panel.add(ad1Button);
+        ad1Button.setBounds(67, 215, 59, 14);
+        panel.add(ad1Button);
            
-           //creo boton end
-           
-           RoundButton nextButton = new RoundButton("End");
-           nextButton.setForeground(Color.BLACK);
-           nextButton.setFont(new Font("SansSerif", Font.BOLD, 9));
-           nextButton.setBackground(Color.GRAY);
-           nextButton.addActionListener(new ActionListener() {
-              	public void actionPerformed(ActionEvent e) {
-              	}          	
-              	//Aqui codigo del evento al pulsar el boton
-              });
-           nextButton.setBounds(303, 215, 72, 26);
-           panel.add(nextButton);
+        //creo boton end  
+        nextButton = new RoundButton("End");
+        nextButton.setForeground(Color.BLACK);
+        nextButton.setFont(new Font("SansSerif", Font.BOLD, 9));
+        nextButton.setBackground(Color.GRAY);
+        nextButton.addActionListener(new ActionListener() {
+        	
+			public void actionPerformed(ActionEvent e) {
+			}
+			// Aqui codigo del evento al pulsar el boton
+		});
+        nextButton.setBounds(303, 215, 72, 26);
+        panel.add(nextButton);
            
 	
-	   //Guardamos el cambio del slider
-	   slider.addChangeListener(new ChangeListener() {
+        //Guardamos el cambio del slider
+        slider.addChangeListener(new ChangeListener() {
         public void stateChanged(ChangeEvent e) {
         int value = slider.getValue()*12;
         sliderValueLabel.setText("Su fianza es de: " + value);
             // Imprimimos en consola valor del slider para prueba
            // System.out.println("Valor del slider: " + value);
-        }
-    });
-	   
+        	}
+        });
 	  
-	   CheckOut.setVisible(true);
-}
+        CheckOutFrame.setVisible(true);
+	}
+
+	public JFrame getCheckOutFrame() {
+		return CheckOutFrame;
+	}
+
+	public void setCheckOutFrame(JFrame checkOutFrame) {
+		CheckOutFrame = checkOutFrame;
+	}
+
 }
--- Proyecto/src/SignUp/SingUp.java
+++ Proyecto/src/SignUp/SingUp.java
@@ -14,15 +14,23 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import MySQL.MySQLConnection;
+
 public class SingUp extends JFrame {
 
-	JFrame SingUp;
+	private JFrame singUpFrame;
 	private JTextField nametextField;
 	private JTextField emailtextField;
 	private JTextField passwordtextField;
-	private JTextField textField;
-	private JTextField textField_1;
-
+	private JTextField eMailTextField;
+	private JTextField passwordTextField;
+	private JPanel panel;
+	private JLabel nameLabel;
+	private JLabel titleLabel;
+	private JLabel imageLabel;
+	private JLabel eMailLabel;
+	private JLabel passwordLabel;
+	private RoundButton nextButton;
 
 	/**
 	 * Launch the application.
@@ -33,7 +41,7 @@ public class SingUp extends JFrame {
 			public void run() {
 				try {
 					SingUp window = new SingUp();
-					       window.SingUp.setVisible(true);
+					       window.singUpFrame.setVisible(true);
 					    
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -53,97 +61,118 @@ public class SingUp extends JFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		SingUp = new JFrame();
-		SingUp.setBounds(100, 100, 450, 300);
-		 setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
+		singUpFrame = new JFrame();
+		singUpFrame.setBounds(100, 100, 450, 300);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
 		
-		 // Crear un panel (JPanel) para agregar contenido.
-        JPanel panel = new JPanel();
+		// Crear un panel (JPanel) para agregar contenido.
+        panel = new JPanel();
 
         //Establecer el color de fondo del panel en amarillo.
         panel.setBackground(Color.YELLOW);
 
         //Agregar el panel a la ventana.
-        SingUp.getContentPane().add(panel, BorderLayout.CENTER);
-           panel.setLayout(null);
+        singUpFrame.getContentPane().add(panel, BorderLayout.CENTER);
+        panel.setLayout(null);
            
         // Crear una etiqueta "Name:"
-           JLabel nameLabel = new JLabel("Name:");
-           nameLabel.setFont(new Font("SansSerif", Font.BOLD, 11));
-           nameLabel.setBounds(70, 76, 33, 14);
-           panel.add(nameLabel);
+        nameLabel = new JLabel("Name:");
+        nameLabel.setFont(new Font("SansSerif", Font.BOLD, 11));
+        nameLabel.setBounds(70, 76, 33, 14);
+        panel.add(nameLabel);
         //Campo para ingresar name
-           nametextField = new JTextField(20);
-           nametextField.setBounds(134, 74, 166, 20);
-           panel.add(nametextField);
+        nametextField = new JTextField(20);
+        nametextField.setBounds(134, 74, 166, 20);
+        panel.add(nametextField);
            
         // Crear una etiqueta (JLabel) para el t�tulo "SIGN UP!"
-           JLabel titleLabel = new JLabel("SIGN UP!");
-           titleLabel.setBounds(151, 11, 133, 32);
-           titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
-           titleLabel.setForeground(Color.BLACK); // Establece el color del texto
-           panel.add(titleLabel); 
-           
-           
-           // Crea una etiqueta para mostrar la imagen
-                 JLabel imageLabel = new JLabel();
-                 imageLabel.setBounds(306, 31, 100, 117); // Ajusta las coordenadas y dimensiones seg�n tus necesidades
-                 panel.add(imageLabel);
-            // Ruta de la imagen
-                 String imagePath = "LOGO CLOUDTOWN-27.png"; // Reemplaza con la ruta correcta de tu imagen
-      try {
-                 // Carga la imagen
-                 ImageIcon imageIcon = new ImageIcon(imagePath);
-                 Image image = imageIcon.getImage();
+        titleLabel = new JLabel("SIGN UP!");
+        titleLabel.setBounds(151, 11, 133, 32);
+        titleLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
+        titleLabel.setForeground(Color.BLACK); // Establece el color del texto
+        panel.add(titleLabel); 
+        
+        // Crea una etiqueta para mostrar la imagen
+        imageLabel = new JLabel();
+        imageLabel.setBounds(306, 31, 100, 117); // Ajusta las coordenadas y dimensiones seg�n tus necesidades
+        panel.add(imageLabel);
+        // Ruta de la imagen
+        String imagePath = "LOGO CLOUDTOWN-27.png"; // Reemplaza con la ruta correcta de tu imagen
+        try {
+            // Carga la imagen
+            ImageIcon imageIcon = new ImageIcon(imagePath);
+            Image image = imageIcon.getImage();
 
-                 // Ajusta el tama�o de la imagen
-                 Image newImage = image.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH);
+            // Ajusta el tama�o de la imagen
+            Image newImage = image.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH);
 
-                 // Actualiza la etiqueta con la nueva imagen
-      imageLabel.setIcon(new ImageIcon(newImage));
-      }catch (Exception ex) {
+            // Actualiza la etiqueta con la nueva imagen
+            imageLabel.setIcon(new ImageIcon(newImage));
+        }catch (Exception ex) {
           ex.printStackTrace();
-      }
+        }
            
         // Crear una etiqueta (JLabel) para el t�tulo "email!"
-           JLabel lblEmail = new JLabel("E-mail:");
-           lblEmail.setFont(new Font("SansSerif", Font.BOLD, 11));
-           lblEmail.setBounds(70, 111, 44, 14);
-           panel.add(lblEmail);
+        eMailLabel = new JLabel("E-mail:");
+        eMailLabel.setFont(new Font("SansSerif", Font.BOLD, 11));
+        eMailLabel.setBounds(70, 111, 44, 14);
+        panel.add(eMailLabel);
            
         // Crear campo para ingresar "email!"
-           textField = new JTextField(20);
-           textField.setBounds(134, 105, 166, 20);
-           panel.add(textField);
+        eMailTextField = new JTextField(20);
+        eMailTextField.setBounds(134, 105, 166, 20);
+        panel.add(eMailTextField);
            
         // Crear campo para ingresar "Password"
-           textField_1 = new JTextField(20);
-           textField_1.setBounds(134, 136, 166, 20);
-           panel.add(textField_1);
+        passwordTextField = new JTextField(20);
+        passwordTextField.setBounds(134, 136, 166, 20);
+        panel.add(passwordTextField);
            
-          // Crear una etiqueta (JLabel) para el t�tulo "Password"          
-           JLabel nameLabel_1_1 = new JLabel("Password:");
-           nameLabel_1_1.setFont(new Font("SansSerif", Font.BOLD, 11));
-           nameLabel_1_1.setBounds(56, 142, 66, 14);
-           panel.add(nameLabel_1_1);
+        // Crear una etiqueta (JLabel) para el t�tulo "Password"          
+        passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(new Font("SansSerif", Font.BOLD, 11));
+        passwordLabel.setBounds(56, 142, 66, 14);
+        panel.add(passwordLabel);
            
-           // Crear un bot�n "Next"
-           RoundButton nextButton = new RoundButton("Next");
-           nextButton.setBackground(Color.GRAY);
-           nextButton.setForeground(new Color(0, 0, 0));
-           nextButton.setFont(new Font("SansSerif", Font.BOLD, 11));
-           nextButton.addActionListener(new ActionListener() {
-           	public void actionPerformed(ActionEvent e){
-           		dispose();  
-           //cerrar la ventana actual (SignUp) antes de abrir la siguiente (SignUp2).	
+        // Crear un bot�n "Next"
+        nextButton = new RoundButton("Next");
+        nextButton.setBackground(Color.GRAY);
+        nextButton.setForeground(new Color(0, 0, 0));
+        nextButton.setFont(new Font("SansSerif", Font.BOLD, 11));
+        nextButton.addActionListener(new ActionListener() {
+        	   
+          	public void actionPerformed(ActionEvent e){
+          		dispose();  
+           		//cerrar la ventana actual (SignUp) antes de abrir la siguiente (SignUp2).
+           		String nombreTabla="registrousuarios";           		
+           		String nombre=nametextField.getText().toString();
+           		String eMail=eMailTextField.getText().toString();
+           		String password=passwordTextField.getText().toString();
+           		           		
+           		MySQLConnection conexion=new MySQLConnection();
+           		
+           		conexion.insertarLineaRegistro(nombreTabla, nombre, eMail, password);
+           		
+           		conexion.desconectar();
            
-           // Crea una instancia de la siguiente ventana (SignUp2) y hazla visible.
-            SingUp2 singUp2 = new SingUp2();
-            singUp2.SingUp2.setVisible(true);
-           	}
-           });
-           nextButton.setBounds(250, 195, 100, 40);
-           panel.add(nextButton);        
+           		// Crea una instancia de la siguiente ventana (SignUp2) y hazla visible.
+           		SingUp2 singUp2 = new SingUp2();
+           		singUp2.getSingUp2Frame().setVisible(true);
+           		}
+           	});
+        nextButton.setBounds(250, 195, 100, 40);
+        panel.add(nextButton);        
            
-	}	
+	}
+
+	public JFrame getSingUpFrame() {
+		return singUpFrame;
+	}
+
+	public void setSingUpFrame(JFrame singUpFrame) {
+		this.singUpFrame = singUpFrame;
+	}
+	
+	
+	
 }
